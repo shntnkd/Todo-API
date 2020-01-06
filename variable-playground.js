16f914c4a48bf72c717a2e7fc0ade84fa5f51a6f
Shantanu Kadu <shntnkd@gmail.com>
2020-01-06 15:11:16 +0530

    add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,24 @@
+/*var person = {
+	name: "Shantanu",
+	age: 27
+};
+
+function updatePerson(obj) {
+	
+	obj.name ="Shantanu";
+	obj.age =21;
+}
+updatePerson(person);
+console.log(person);
+
+*/
+
+// Array Example
+
+var grades = [15,27];
+function addGrades(gradeArr) {
+	gradeArr.push(29);
+	debugger;
+}
+
+console.log(grades);
